c8da5bb3bb1a2c71543e0ddfa949ed83ca935268
Lab_s <surachet2421@gmail.com>
2026-03-29 10:11:17 +0700

    Add EventListener btn

--- UBCHECK/myapp/static/js/scripts.js
+++ UBCHECK/myapp/static/js/scripts.js
@@ -1,12 +1,35 @@
-const btn = document.getElementById("my_btn");
-const details = document.getElementById("details")
+const allRows = document.querySelectorAll('.more-row');
+const btn = document.getElementById('my_btn')
 
-btn.addEventListener('click', ()=> {
-    details.classList.toggle('d-none')
+btn.addEventListener('click', ()=>{
+    const anyopen = document.querySelector('.details.show')
 
-    if (details.classList.contains('d-none')){
-        btn.innerText = 'แสดงรายละเอียดเพิ่มเติม'
-    }else{
-        btn.inerText = 'ซ่อนรายละเอียด'
-    }
+    allRows.forEach(row=>{
+        const details = row.querySelector('.details');
+
+        if (anyopen){
+            details.classList.remove('show')
+        }else{
+            details.classList.add('show')
+        }
+
+    })
+    btn.innerText = anyopen ? 'แสดงข้อมูลเพิ่มเติม' : 'แสดงน้อยลง'
 })
+
+allRows.forEach(row =>{
+    const details = row.querySelector('.details');
+
+    row.addEventListener('click', ()=>{
+        details.classList.toggle('show');
+
+        const isopen = document.querySelector('.details.show');
+        btn.innerText = isopen ? 'แสดงน้อยลง' : 'แสดงข้อมูลเพิ่มเติม'
+    })
+
+    details.addEventListener('click', (e) => {
+        e.stopPropagation(); // หยุดการส่งเหตุการณ์คลิกไปหาตัวพ่อ (row)
+    });
+})
+
+
