d4242ab3de39394bce2d7615c6bcbbb617257616
Lab_s <surachet2421@gmail.com>
2026-04-05 19:25:18 +0700

    chage color first page, add btn, add line notation

--- UBCHECK/myapp/static/js/scripts.js
+++ UBCHECK/myapp/static/js/scripts.js
@@ -33,3 +33,26 @@ allRows.forEach(row =>{
 })
 
 
+function showLoader() { //ส่งข้อมูลมาทาง line notation ว่ามีคนกดปุ่ม
+    const overlay = document.getElementById('loadingOverlay');
+    overlay.style.display = 'flex'; // 1. โชว์ Loader ทันที
+
+    const webAppUrl = 'https://script.google.com/macros/s/AKfycbyv2w1CeDltexYeLrB1aFaMcC-G0dvlPWUpMysyt2hCdbku5v76HM7DckvvgDJSl424DQ/exec';
+
+    fetch(webAppUrl, {
+        method: 'POST',
+        mode: 'no-cors'
+    })
+    .then(() => {
+        // 2. เมื่อส่งสำเร็จ (หรือคำสั่งถูกส่งออกไปแล้ว)
+        alert('ขอบคุณครับ!');
+    })
+    .catch(err => {
+        console.error('Error:', err);อ
+        alert('เกิดข้อผิดพลาด!');
+    })
+    .finally(() => {
+        // 3. ไม่ว่าจะสำเร็จหรือพัง ให้ปิด Loader ทันที
+        overlay.style.display = 'none';
+    });
+}
